ab809d7525850cf0889a77fd022cc8c979a5cbe8
ryuneng <137076160+ryuneng@users.noreply.github.com>
2024-04-09 15:20:31 +0900

    Feat: main.html 파일 추가

--- src/main/java/com/roommake/main/MainController.java
+++ src/main/java/com/roommake/main/MainController.java
@@ -1,4 +1,13 @@
 package com.roommake.main;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MainController {
+
+    @GetMapping("/")
+    public String main () {
+        return "main";
+    }
 }
