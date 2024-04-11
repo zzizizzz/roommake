a17e34fd5b8036bb34ea221f45a7064f061c93e9
ryuneng <137076160+ryuneng@users.noreply.github.com>
2024-04-11 20:27:13 +0900

    Feat: 주문/결제 화면 UI 구현

--- src/main/java/com/roommake/cart/CartController.java
+++ src/main/java/com/roommake/cart/CartController.java
@@ -2,6 +2,7 @@ package com.roommake.cart;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CartController {
@@ -9,5 +10,10 @@ public class CartController {
     @GetMapping("/cart")
     public String cart() {
         return "cart/cart";
+    }
+
+    @PostMapping("/orderform")
+    public String orderform() {
+        return "order/orderform";
     }
 }
