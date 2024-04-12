f88e53c69fe7becf3ff64c6921e973ec3adb43e9
Dayeongs <ekdud9636@gmail.com>
2024-04-12 11:52:37 +0900

    Feat: 채널 생성, 글 등록, 커뮤니티 글 등록 화면 UI 구현

--- src/main/java/com/roommake/channel/ChannelPostController.java
+++ src/main/java/com/roommake/channel/ChannelPostController.java
@@ -1,21 +1,21 @@
-package com.roommake.community;
+package com.roommake.channel;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/community")
-public class CommunityController {
+@RequestMapping("/channel/post")
+public class ChannelPostController {
 
     @GetMapping("/list")
     public String list() {
-        return "knowhow-list";
+        return "channel/post/list";
     }
 
     @GetMapping("/form")
-    public String create() {
-        return "community/form";
+    public String createPost() {
+        return "channel/post/form";
     }
 
 }
