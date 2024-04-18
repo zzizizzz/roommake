d415b508eeca76dd29789a688452794aaf25244a
namhyunzi <hyunji9886@knou.ac.kr>
2024-04-18 15:30:33 +0900

    Refactor: User 테이블 수정에 따른 vo, dto 수정
    - User.java 변수 수정 및 추가
    - UserSignupForm.java 변수 추가

--- src/main/java/com/roommake/user/dto/UserSignupForm.java
+++ src/main/java/com/roommake/user/dto/UserSignupForm.java
@@ -9,9 +9,9 @@ import lombok.Setter;
 @Getter
 @Setter
 public class UserSignupForm {
-    private String email;
-    private String password;
-    private String confirmPassword;
-    private String nickname;
-
+    private String email;               // 유저 이메일
+    private String password;            // 유저 비밀번호
+    private String confirmPassword;     // 유저 비밀번호 확인
+    private String nickname;            // 유저 닉네임
+    private String optionRecommendCode; // 추천코드 (가입시 선택 입력)
 }
--- src/main/java/com/roommake/user/vo/User.java
+++ src/main/java/com/roommake/user/vo/User.java
@@ -11,25 +11,25 @@ import java.util.Date;
 @ToString
 public class User {
 
-    private int id;               // 유저 번호
-    private String email;         // 유저 이메일(아이디)
-    private String password;      // 유저 비밀번호
-    private String nickname;      // 유저 닉네임
-    private String profilePhoto;  // 유저 프로필사진
-    private String tel;           // 유저 연락처
-    private Date birthDate;       // 유저 생일
-    private String introduction;  // 유저 소개
-    private String sns;           // 유저 SNS
-    private Date createDate;      // 유저 가입일
-    private Date updateDate;      // 유저 수정일
-    private Date deleteDate;      // 유저 탈퇴일
-    private String recommendCode; // 유저 추천코드
-    private String socialYn;      // 유저 소셜 로그인 여부
-    private String status;        // 유저 상태
-    private UserGrade gradeId;    // 등급번호
-    private int point;            // 유저 보유포인트
-    private String address;       // 유저 주소
-    private int complaintCount;   // 유저 신고 누적수
-    private int followingCount;   // 유저 팔로잉 수
-    private int followerCount;    // 유저 팔로워 수
+    private int id;                      // 유저 번호
+    private String email;                // 유저 이메일(아이디)
+    private String password;             // 유저 비밀번호
+    private String nickname;             // 유저 닉네임
+    private String profilePhoto;         // 유저 프로필사진
+    private String tel;                  // 유저 연락처
+    private Date birthDate;              // 유저 생일
+    private String introduction;         // 유저 소개
+    private String sns;                  // 유저 SNS
+    private Date createDate;             // 유저 가입일
+    private Date updateDate;             // 유저 탈퇴일
+    private String uniqueRecommendCode;  // 유저 추천코드 (가입시 자동부여)
+    private String socialYn;             // 유저 소셜 로그인 여부
+    private String status;               // 유저 상태
+    private UserGrade gradeId;           // 등급번호
+    private int point;                   // 유저 보유포인트
+    private String address;              // 유저 주소
+    private int complaintCount;          // 유저 신고 누적수
+    private int followingCount;          // 유저 팔로잉 수
+    private int followerCount;           // 유저 팔로워 수
+    private String optionRecommendCode;  // 유저 추천코드 (가입시 선택입력)
 }
