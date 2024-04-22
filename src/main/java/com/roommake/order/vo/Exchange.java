70ff9a5f2cea1aac14c81d86ba4b2ddaef5864eb
ryuneng <137076160+ryuneng@users.noreply.github.com>
2024-04-22 15:12:50 +0900

    Refactor: 반품교환 관련 DB 수정에 따른 vo 수정
    - 반품사유/교환사유 분리되어 있던 테이블 하나로 통합
    - 반품/교환사유 테이블에 상세사유 컬럼 추가 (Nullable)
    - 반품 테이블 : 상품회수지 컬럼 추가
    - 교환 테이블 : 상품회수지, 재배송지 컬럼 추가

--- src/main/java/com/roommake/order/vo/Exchange.java
+++ src/main/java/com/roommake/order/vo/Exchange.java
@@ -11,11 +11,13 @@ import java.util.Date;
 @ToString
 public class Exchange {
 
-    private int id;                             // 교환번호
-    private String status;                      // 교환처리상태
-    private OrderItem orderItemId;              // 주문상세번호
-    private String approvalYn;                  // 교환승인여부
-    private ExchangeReason ReasonId;            // 교환사유번호
-    private Date createDate;                    // 교환생성일
-    private Date updateDate;                    // 교환수정일
+    private int id;                      // 교환번호
+    private String status;               // 교환처리상태
+    private OrderItem orderItemId;       // 주문상세번호
+    private String approvalYn;           // 교환승인여부
+    private ReturnExchangeReason reason; // 교환사유번호
+    private Date createDate;             // 교환생성일
+    private Date updateDate;             // 교환수정일
+    private Delivery deliveryCollection; // 상품 회수지 번호
+    private Delivery reDelivery;         // 상품 재배송지 번호
 }
