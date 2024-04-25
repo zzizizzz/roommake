bcfeabf2b7cf72ba124ed9053c7f18a8d26fbbfc
Dayeongs <ekdud9636@gmail.com>
2024-04-25 14:05:55 +0900

    Fix: 로그인하지 않은 사용자가 로그인이 필요한 페이지에 접속시 로그인 페이지로 이동

--- src/main/java/com/roommake/user/security/LoginUser.java
+++ src/main/java/com/roommake/user/security/LoginUser.java
@@ -2,12 +2,18 @@ package com.roommake.user.security;
 
 public abstract class LoginUser {
     private int id;
+    private String nickname;
 
-    public LoginUser(int id) {
+    public LoginUser(int id, String nickname) {
         this.id = id;
+        this.nickname = nickname;
     }
 
     public int getId() {
         return id;
+    }
+
+    public String getNickname() {
+        return nickname;
     }
 }
