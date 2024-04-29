1bcef7c439f95cb378d65c7867baee55c8a3c273
Hey-Yujin365 <realgyj@gmail.com>
2024-04-29 14:40:18 +0900

    Feat: getUsers 구현 및 user관련 enum 추가(페이징, 정렬, 필터링, 검색 기능 추가된 버전의 getAllUsers)
    getAllUsers로는 페이징 관련 처리 불가하여 getUsers 추가

---
+++ src/main/java/com/roommake/user/emuns/UserStatusEnum.java
@@ -0,0 +1,19 @@
+package com.roommake.user.emuns;
+
+import lombok.Getter;
+
+@Getter
+public enum UserStatusEnum {
+
+    ACTIVE("active"), BLOCK("block"), DELETE("delete");
+
+    private final String status;
+
+    private UserStatusEnum(String status) {
+        this.status = status;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+}
