5e9e17442719d5fef0665ad6e92020049376fc35
wp2378 <wp2378@naver.com>
2024-04-29 19:54:08 +0900

    Feat: 상품디테일페이지 댓글,별점 CRUD기능 구현(60%)

---
+++ src/main/java/com/roommake/product/dto/ProductReviewDto.java
@@ -0,0 +1,22 @@
+package com.roommake.product.dto;
+
+import com.roommake.product.vo.Product;
+import com.roommake.product.vo.ProductReview;
+import com.roommake.user.vo.User;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+
+public class ProductReviewDto {
+
+    private int id;
+    private String content;
+    private int rating;
+    private Date createDate;
+    private int voteCount;
+    private String nickname;
+}
