9ac03146db5f96cf789d9475cdb331bf2054cbd8
ryuneng <137076160+ryuneng@users.noreply.github.com>
2024-04-29 11:38:10 +0900

    Feat: 카카오페이 실제 데이터로 전송 및 주문 Insert 구현
    - 결제, 주문상세 Insert 구현 예정
    - 장바구니 삭제 확인 알림창 표시
    - 기본배송지 여부에 따라 주문 폼, 배송지 수정 폼 표시 내용 일부 변경
    - properties > Mapper 로그 레벨 추가

---
+++ src/main/java/com/roommake/order/dto/OrderCreateForm.java
@@ -0,0 +1,20 @@
+package com.roommake.order.dto;
+
+import com.roommake.cart.dto.CartCreateForm;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+
+@Getter
+@Setter
+@ToString
+public class OrderCreateForm {
+
+    private String name;         // 카카오페이에 보낼 대표 상품명
+    private int totalPrice;      // 총 결제금액
+    private int usedPoint;       // 사용 포인트
+    private int deliveryId;      // 배송지 번호
+    List<CartCreateForm> items;  // 상품번호, 상품상세번호, 상품수량이 담긴 객체 배열
+}
--- src/main/java/com/roommake/order/mapper/OrderMapper.java
+++ src/main/java/com/roommake/order/mapper/OrderMapper.java
@@ -2,8 +2,10 @@ package com.roommake.order.mapper;
 
 import com.roommake.cart.dto.CartCreateForm;
 import com.roommake.cart.dto.CartItemDto;
+import com.roommake.order.dto.OrderCreateForm;
 import com.roommake.order.vo.Delivery;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface OrderMapper {
@@ -11,4 +13,8 @@ public interface OrderMapper {
     CartItemDto getProductsByDetailId(CartCreateForm cartCreateForm);
 
     Delivery getDefaultDeliveryByUserId(int id);
+
+    CartItemDto getProductDetailByDetailId(int id);
+
+    void createOrder(@Param("form") OrderCreateForm orderCreateForm, @Param("userId") int userId);
 }
