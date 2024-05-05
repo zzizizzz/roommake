17bca68b71104cf92a1f375e293891ffc45b8644
Dayeongs <ekdud9636@gmail.com>
2024-05-05 10:58:45 +0900

    Feat: 커뮤니티 글 (추천 커뮤니티 글 조회, 신고) / 댓글 (등록, 목록 조회, 페이징, 신고) 기능 구현
    - 다른 커뮤니티글 추천
    - 커뮤니티 글 신고
    - 댓글 등록
    - 댓글 목록 조회
    - 댓글 페이징
    - 댓글 신고

--- src/main/java/com/roommake/community/dto/CommDetailDto.java
+++ src/main/java/com/roommake/community/dto/CommDetailDto.java
@@ -2,6 +2,7 @@ package com.roommake.community.dto;
 
 import com.roommake.community.vo.Community;
 import com.roommake.community.vo.ComplaintCategory;
+import com.roommake.dto.Pagination;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,8 +11,12 @@ import java.util.List;
 @Getter
 @Setter
 public class CommDetailDto {
-    List<ComplaintCategory> complaintCategories;
-    Community community;
+    private List<ComplaintCategory> complaintCategories;
+    private Community community;
     private boolean isLike;
     private boolean isScrap;
+    private int totalReplyCount;
+    private List<CommReplyDto> communityReplies;
+    private Pagination replyPagination;
+    private List<Community> recommendCommunities;
 }
