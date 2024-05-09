280844d62b28bfb47fe895cbd3b37977f2b6c76f
ryuneng <137076160+ryuneng@users.noreply.github.com>
2024-05-09 11:44:00 +0900

    Feat: 주문취소 시 사용한 포인트 반환 및 유저 보유포인트 갱신

--- src/main/java/com/roommake/user/enums/PointReasonEnum.java
+++ src/main/java/com/roommake/user/enums/PointReasonEnum.java
@@ -5,7 +5,11 @@ import lombok.Getter;
 @Getter
 public enum PointReasonEnum {
 
-    DAILY_CHECK("출석체크"), SIGN_UP("회원가입으로 인한 적립"), RECOMMEND("추천인가입으로 인한 적립"), CONFIRM_ORDER("상품구매확정 - 주문번호 : ");
+    DAILY_CHECK("출석체크"),
+    SIGN_UP("회원가입으로 인한 적립"),
+    RECOMMEND("추천인가입으로 인한 적립"),
+    CONFIRM_ORDER("상품구매확정 - 주문번호 : "),
+    CANCEL_ORDER("취소한 주문번호 : ");
 
     private final String reason;
 
